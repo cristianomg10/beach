7985628b890cae328c22216c35593a22056be561
Cristiano Garcia <cristiano@CRISTIANO.(none)>
2016-11-28 14:40:30 -0200

    Refactoring a method in ArtificialBeeColony and inserting License.

--- src/ArtificialBeeColony/ArtificialBeeColony.php
+++ src/ArtificialBeeColony/ArtificialBeeColony.php
@@ -20,8 +20,6 @@ class ArtificialBeeColony implements IOptimizer
 {
     private $nBees;
     private $nIterations;
-    /*private $c1;
-    private $c2;*/
     private $nDimensions;
     private $objectiveFunction;
     private $bees;
@@ -34,6 +32,7 @@ class ArtificialBeeColony implements IOptimizer
     private $nOnlookers;
     private $a; // acceleration coefficient
     private $selection;
+    private $abandonment;
 
     function __construct($nBees, $nOnlookers, $nIterations, $nDimensions, IObjectiveFunction $objectiveFunction, $min, $max, ILoggable $loggable, ISelection $selection, $a = 1)
     {
@@ -77,7 +76,7 @@ class ArtificialBeeColony implements IOptimizer
     public function run()
     {
         $this->initialize();
-        $abandonment = [];
+        $this->abandonment = Math::generateRandomVector($this->nBees, 0);
         $abandonmentLimit = 0.6 * $this->nBees * $this->nDimensions;
 
         for ($i = 0; $i < $this->nIterations; ++$i) {
@@ -85,85 +84,26 @@ class ArtificialBeeColony implements IOptimizer
             for ($j = 0; $j < $this->nBees; ++$j) {
 
                 $k = $j;
-                //select random index
-                while ($k == $j) $k = round(Math::getRandomValue($this->nBees - 1));
-
-                $part1 = Math::generateRandomVector($this->nDimensions, 2);
-                $part2 = Math::sumNumberToMatrix($part1, -1);
-                $phi = Math::multiplyNumberToMatrix($part2, $this->a);
-
-                $currentBeePosition = new Matrix([$this->bees[$j]->getPosition()]);
-                $kPosition = new Matrix([$this->bees[$k]->getPosition()]);
-
-                $diff = $currentBeePosition->subtract($kPosition);
-
-                $partPhi = (new Matrix([$phi]))->hadamardProduct($diff);
-                $position = $currentBeePosition->add($partPhi)->getRow(0);
-                $newBee = new Bee($position);
-                $newBee->setFitness($this->objectiveFunction->compute($newBee->getPosition()));
-
-                if ($newBee->getFitness() < $this->bees[$j]->getFitness()) {
-                    $this->bees[$j] = $newBee;
-                } else {
-                    if (is_null($abandonment[$j])) {
-                        $abandonment[$j] = 1;
-                    } else {
-                        ++$abandonment[$j];
-                    }
-                }
+                $this->updatePositions($k, $j);
             }
 
             //onlooker bees
             $this->updateFitnesses();
 
-            /*$f = [];
-            $prob = [];
-            $mean = array_sum($this->fitness) / $this->nBees;
-            for ($j = 0; $j < $this->nBees; ++$j){
-                $f[$j] = exp(- $this->bees[$j]->getFitness() / $mean);
-                $prob[$j] = $f / array_sum($f);
-            }*/
-
             for ($j = 0; $j < $this->nBees; ++$j) {
-
-                //select random index
                 $m = $this->selection->select($this->fitness);
                 $k = $m;
-                while ($k == $m) $k = round(Math::getRandomValue($this->nBees - 1));
-
-                $part1 = Math::generateRandomVector($this->nDimensions, 2);
-                $part2 = Math::sumNumberToMatrix($part1, -1);
-                $phi = Math::multiplyNumberToMatrix($part2, $this->a);
-
-                $onLookerBeePosition = new Matrix([$this->bees[$m]->getPosition()]);
-                $kPosition = new Matrix([$this->bees[$k]->getPosition()]);
-
-                $diff = $onLookerBeePosition->subtract($kPosition);
-
-                $partPhi = (new Matrix([$phi]))->hadamardProduct($diff);
-                $position = $onLookerBeePosition->add($partPhi)->getRow(0);
-                $newBee = new Bee($position);
-                $newBee->setFitness($this->objectiveFunction->compute($newBee->getPosition()));
-
-                if ($newBee->getFitness() < $this->bees[$m]->getFitness()) {
-                    $this->bees[$m] = $newBee;
-                } else {
-                    if (is_null($abandonment[$m])) {
-                        $abandonment[$m] = 1;
-                    } else {
-                        ++$abandonment[$m];
-                    }
-                }
+                $this->updatePositions($k, $m);
             }
 
             //scout bees
             for ($j = 0; $j < $this->nBees; ++$j) {
-                if ($abandonment[$j] >= $abandonmentLimit) {
+                if ($this->abandonment[$j] >= $abandonmentLimit) {
                     $this->bees[$j]->setPosition(Math::sumNumberToMatrix(Math::generateRandomVector($this->nDimensions, $this->max * 2), -abs($this->min)));
                     $this->bees[$j]->setFitness($this->objectiveFunction($this->bees[$j]->getFitness()));
                     $this->fitness[$j] = $this->bees[$j]->getFitness();
                 }
-                $abandonment[$j] = 0;
+                $this->abandonment[$j] = 0;
             }
 
             // update best solution;
@@ -175,6 +115,31 @@ class ArtificialBeeColony implements IOptimizer
             }
 
             $this->loggable->write("Iteration $i: Best Fitness: {$this->bestFitness}");
+        }
+    }
+
+    private function updatePositions($k, $m){
+
+        while ($k == $m) $k = round(Math::getRandomValue($this->nBees - 1));
+
+        $part1 = Math::generateRandomVector($this->nDimensions, 2);
+        $part2 = Math::sumNumberToMatrix($part1, -1);
+        $phi = Math::multiplyNumberToMatrix($part2, $this->a);
+
+        $onLookerBeePosition = new Matrix([$this->bees[$m]->getPosition()]);
+        $kPosition = new Matrix([$this->bees[$k]->getPosition()]);
+
+        $diff = $onLookerBeePosition->subtract($kPosition);
+
+        $partPhi = (new Matrix([$phi]))->hadamardProduct($diff);
+        $position = $onLookerBeePosition->add($partPhi)->getRow(0);
+        $newBee = new Bee($position);
+        $newBee->setFitness($this->objectiveFunction->compute($newBee->getPosition()));
+
+        if ($newBee->getFitness() < $this->bees[$m]->getFitness()) {
+            $this->bees[$m] = $newBee;
+        } else {
+            ++$this->abandonment[$m];
         }
     }
 
